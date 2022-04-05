7ff74287acf6d8939cac7f6f95ca57b4abf51cfe
cwpark410 <tonipark76@gmail.com>
2022-04-06 00:41:25 +0900

    api added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import React, { useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import styles from "styles/Home.module.css";
-
+import Link from "next/link";
+import prisma  from "lib/prisma";
+import  api from  'utils/api'
 const FatChart = dynamic(() => import("components/chart"), {
   ssr: false,
 });
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `http://localhost:3000/api/hacker/detail/${context.params.id}`
+  );
+  const hackerDetail = await res.json();
+
+  const res1 = await fetch(
+    `http://localhost:3000/api/replay/${context.params.id}`
+  );
+  const replay = await res1.json();
+  return { props: { hacker: { ...hackerDetail }, replay: [...replay] } };
+
 
 export default function Home() {
   return <FatChart />;
